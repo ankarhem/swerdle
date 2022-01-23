960828e3a41555e8a4a73ef25fe2bcf9dffe4864
Jakob Ankarhem <jakob.ankarhem@outlook.com>
2022-01-23 12:46:46 +0100

    swe header

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -3,16 +3,22 @@
 </script>
 
 <div
-	class="px-3 py-0.5 pt-safe bg-green-100  border-b-[3px] border-b-green-400 flex justify-center items-center"
+	class="px-3 py-0.5 pt-safe bg-green-100  border-b-[3px] border-b-green-400 flex justify-between items-center"
 >
 	<a href="/hjalp" class="text-gray-500">
 		<Icon src={QuestionMarkCircle} size="1.5rem" />
 	</a>
-	<h1 class="hover:cursor-pointer text-5xl uppercase text-gray-500 font-abril mx-auto">
-		<a href="/">
-			<span class="text-green-500">Ordlig</span>
-		</a>
-	</h1>
+	<div class="flex items-center flex-col uppercase text-gray-500 ">
+		<h1 class="hover:cursor-pointer text-5xl font-abril mx-auto">
+			<a href="/" class="flex font-bold">
+				<span class="text-blue-400">S</span>
+				<span class="text-yellow-400">W</span>
+				<span class="text-blue-400">E</span>
+				<span class="text-green-500">rdle</span>
+			</a>
+		</h1>
+		<div class="text-xs block translate-y-[-0.2rem] transform-gpu">Wordle på svenska</div>
+	</div>
 	<a href="/statistik" class="text-gray-500">
 		<Icon src={ChartBar} size="1.5rem" />
 	</a>
--- src/components/Keyboard/Key.svelte
+++ src/components/Keyboard/Key.svelte
@@ -1,11 +1,16 @@
 <script lang="ts">
 	export let specialKey: boolean = false;
+
+	const handleClick: svelte.JSX.MouseEventHandler<HTMLButtonElement> = (event) => {
+		console.log(event.currentTarget.innerText);
+	};
 </script>
 
 <button
 	class={`flex items-center justify-center py-4 rounded uppercase text-white font-bold  ${
 		specialKey === false ? 'w-10 bg-slate-400' : 'w-20 bg-slate-500'
 	}`}
+	on:click={handleClick}
 >
 	<slot />
 </button>

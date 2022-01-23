7854547d51db5e892ab1c64d9d10cb79ec9f5034
Jakob Ankarhem <jakob.ankarhem@outlook.com>
2022-01-24 00:40:28 +0100

    add animation

--- src/components/Tile.svelte
+++ src/components/Tile.svelte
@@ -8,30 +8,46 @@
 </script>
 
 <script lang="ts">
+	import { quadOut } from 'svelte/easing';
 	export let character: string;
-
 	export let state: CharState;
 
 	const getStateStyles = (state) => {
 		switch (state) {
 			case CharState.Correct:
-				return 'bg-green-400 text-primary-500 border-green-500 bg-opacity-50 border-opacity-50';
+				return 'bg-green-300 text-green-600 border-green-600';
 			case CharState.WrongPlace:
-				return 'bg-yellow-400 text-primary-500 border-yellow-500 bg-opacity-50 border-opacity-50';
+				return 'bg-yellow-300 text-yellow-600 border-yellow-600';
 			case CharState.Incorrect:
-				return 'bg-neutral-400 text-primary-500 border-neutral-500 bg-opacity-50 border-opacity-50';
+				return 'bg-neutral-300 text-neutral-600 border-neutral-600';
 			case CharState.Unknown:
-				return 'bg-neutral-50 text-primary-500 border-primary-300';
+				return 'bg-neutral-50 text-neutral-500 border-neutral-300';
 			default:
 				return '';
 		}
 	};
 
 	$: stateStyles = getStateStyles(state);
+
+	function expand(node, params) {
+		const existingTransform = getComputedStyle(node).transform.replace('none', '');
+
+		if (character.length === 0) return;
+
+		return {
+			delay: 0,
+			duration: 100,
+			easing: quadOut,
+			css: (t, u) => `transform: ${existingTransform} scale(${1 + 0.3 * (t > 0.5 ? 1 - t : 1 - u)})`
+		};
+	}
 </script>
 
-<div
-	class={`text-5xl w-16 h-16 rounded transition-colors border-2  uppercase font-bold flex items-center justify-center ${stateStyles}`}
->
-	{character}
-</div>
+{#key character.toLowerCase()}
+	<div
+		in:expand
+		class={`text-5xl w-16 h-16 rounded transition-colors border-2 uppercase font-bold flex items-center justify-center ${stateStyles}`}
+	>
+		{character}
+	</div>
+{/key}

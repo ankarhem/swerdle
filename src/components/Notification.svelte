8f1a3dff55bbf684f0db42e1c239b37a7efe1248
Jakob Ankarhem <jakob.ankarhem@outlook.com>
2022-02-03 00:51:35 +0100

    add dark for notification

--- src/components/Notification.svelte
+++ src/components/Notification.svelte
@@ -12,13 +12,13 @@
 	const getNotificationStyles = (type?: 'success' | 'warning' | 'error') => {
 		switch (type) {
 			case 'success':
-				return 'bg-green-300 text-neutral-600 border-green-600';
+				return 'bg-green-300 text-green-600 border-green-600 dark:bg-green-700 dark:text-green-400 dark:border-green-400';
 			case 'error':
-				return 'bg-red-300 text-neutral-600 border-red-600';
+				return 'bg-red-300 text-neutral-600 border-red-600 dark:bg-red-700 dark:text-red-400 dark:border-red-400';
 			case 'warning':
-				return 'bg-yellow-300 text-neutral-600 border-yellow-600';
+				return 'bg-yellow-300 text-yellow-600 border-yellow-600 dark:bg-yellow-700 dark:text-yellow-500 dark:border-yellow-500';
 			default:
-				return 'bg-neutral-300 text-neutral-600 border-neutral-600';
+				return 'bg-neutral-300 text-neutral-600 border-neutral-600 dark:bg-neutral-700 dark:text-neutral-400 dark:border-neutral-400';
 		}
 	};
 </script>
@@ -28,7 +28,7 @@
 		y: -35,
 		easing: cubicInOut
 	}}
-	class={`uppercase font-bold translate-y-32 whitespace-nowrap flex h-9 p-4 border-2 items-center rounded ${getNotificationStyles(
+	class={`uppercase font-bold translate-y-32 whitespace-nowrap flex h-12 text-xl p-4 border-2 items-center rounded ${getNotificationStyles(
 		notification.type
 	)}`}
 >
